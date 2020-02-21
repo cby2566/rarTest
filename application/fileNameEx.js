5bf867eaef29177650f1f254bfdbd2595f51487c
cby2566 <870017005@qq.com>
2020-02-22 00:22:53 +0800

    map做文件名去重

---
+++ application/fileNameEx.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const fsPromise = fs.promises;
+
+function readAllFileName(url){
+   return fsPromise.readdir(url)
+    .then((data)=>{
+        // console.log(data)
+        return data;
+    }).catch((err)=>{
+        console.log('err:',err)
+    })
+}
+
+function filterName(nameArr){
+    if(!Array.isArray(nameArr)) return;
+    let reg = /\[(.+?)\]/g;
+    let regs = /[\[\]]/g; 
+    let arrObj = [];
+    
+    for(let item of nameArr){
+        // console.log(item.match(reg))
+        let tag = item.match(reg);
+        if(tag){
+            tag = tag.map((i)=>{
+                return i.replace(regs,'');
+            })
+            arrObj.push(tag[0]);
+        }
+    }
+    ///
+    let author = new Map();
+    let reg_ = /\((.+?)\)/g;
+    let regs_ = /\(|\)/g;//不知道为什么要加中线
+
+    for(let list of arrObj){
+        let text = list.match(reg_) ? list.match(reg_) : [`(${list})`];
+        let value = list.replace(text,'').trim().split('  ');
+        let key = text[0].replace(regs_,'');
+        value.push(key);
+        author.set(key,value);
+    }
+    return author
+}
+// readAllFileName('/菊姬')
+async function start(){
+    let fileNameArr = await readAllFileName('/菊姬zip')
+    let arr1 = filterName(fileNameArr)
+    fileNameArr = await readAllFileName('/菊姬')
+    let arr2 = filterName(fileNameArr)
+    // console.log(arr2)
+    for(let i of arr1.keys()){
+        if(arr2.has(i)){
+            console.log(i,arr2.get(i))
+        }
+    }
+    //交集
+    //let intersection = arr1.filter( v => arr2.includes(v))
+}
+start()
--- regExp.js
+++ regExp.js
@@ -1,9 +1,10 @@
 //正则
-let text = ""
+let text = "[感電少女注意報 (真冬)] 蛇神の巫女貳 (C97) モリサマーをペロペロしたい。等23本合集[1.11G+338MFm百度]"
+let text2 = "doumou"
 let reg = /\[(.+?)\]/g
 //.+ 贪婪匹配
 //.+? 惰性匹配 这里需要用这个
 
 let result = null
 
-console.log(text.match(reg))
+console.log(text2.match(reg))
