1833f28554e9dca2bd9ecab45dc8db415275ddc6
cby2566 <870017005@qq.com>
2020-04-26 14:18:04 +0800

    剔除部分代码

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    database: 'soul_plus', // 使用哪个数据库
-    user: 'root', // 用户名
-    password: '123456', // 口令
-    host: 'localhost', // 主机名
-};
-
-module.exports = config;
--- spn.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-http.createServer(function(request,response){
-    response.header("Access-Control-Allow-Origin","*")
-    response.header("Access-Control-Allow-Headers","*")
-    response.header("Access-Control-Allow-Methods","*")
-    console.log(101)
-    response.end('999')
-}).listen(9500)
